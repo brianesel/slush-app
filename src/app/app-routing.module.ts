c4138ced6bfaded4ee7ae537586755511a5292f7
trungnguyen <trunng@metropolia.fi>
2020-02-13 12:44:39 +0200

    add map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {path: 'home', component: HomeComponent}
+    ]
+  },
+  {
+    path: 'map',
+    component: MapComponent,
+    children: [
+      
+    ]
+  }   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { MapComponent } from './map/map.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdminMapComponent } from './admin-map/admin-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigatorComponent,
+    MapComponent,
+    SigninComponent,
+    SignupComponent,
+    AdminMapComponent
   ],
   imports: [
     BrowserModule,
